515ef72c8684a88dc4d15056371b1d8aefc35211
Aira Estur <airaaltisestur@gmail.com>
2024-01-09 21:47:00 +0800

    initial commit

---
+++ Guitar-Chords-Website/Initial_WebsiteDesign/data.js
@@ -0,0 +1,102 @@
+// filter categories
+const categories = [
+  {
+    name: 'All',
+    filter: 'all',
+  },
+  {
+    name: 'Major',
+    filter: 'major',
+  },
+  {
+    name: 'Minor',
+    filter: 'minor',
+  },
+];
+
+const images = [
+  {
+    id: '1',
+    url:'chords/majors/a.svg',
+    category: 'major',
+    title: 'Wave',
+  },
+  {
+    id: '2',
+    url: 'chords/majors/B.svg',
+    category: 'major',
+    title: 'Aurora',
+  },
+  {
+    id: '3',
+    url: 'chords/majors/C.svg',
+    category: 'major',
+    title: 'Wedding 2',
+  },
+  {
+    id: '4',
+    url: 'chords/majors/D.svg',
+    category: 'major',
+    title: 'Beach and Sea from drone',
+  },
+  {
+    id: '5',
+    url: 'chords/majors/E.svg',
+    category: 'major',
+    title: 'Mountain',
+  },
+  {
+    id: '6',
+    url: 'chords/majors/F.svg',
+    category: 'major',
+    title: 'Road from Drone',
+  },
+  {
+    id: '7',
+    url: 'chords/majors/G.svg',
+    category: 'major',
+    title: 'minor 1',
+  },
+  {
+    id: '8',
+    url: 'chords/minors/A minor.svg',
+    category: 'minor',
+    title: 'Wedding 1',
+  },
+  {
+    id: '9',
+    url: 'chords/minors/B minor.svg',
+    category: 'minor',
+    title: 'Hills',
+  },
+  {
+    id: '11',
+    url: 'chords/minors/C minor.svg',
+    category: 'minor',
+    title: 'minor 2',
+  },
+  {
+    id: '12',
+    url: 'chords/minors/D minor.svg',
+    category: 'minor',
+    title: 'Lake',
+  },
+  {
+    id: '13',
+    url: 'chords/minors/E minor.svg',
+    category: 'minor',
+    title: 'major lake',
+  },
+  {
+    id: '14',
+    url: 'chords/minors/F minor.svg',
+    category: 'minor',
+    title: 'Mountain Snowy',
+  },
+  {
+    id: '15',
+    url: 'chords/minors/G minor.svg',
+    category: 'minor',
+    title: 'Aurora2',
+  },
+];
